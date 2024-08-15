2491a473af07628eaded72b0e3b5ccc667b7ea40
yadavgopinath <yadavgopinath93@gmail.com>
2024-08-16 00:23:00 +0530

    creating-server2

--- my-server/app.js
+++ my-server/app.js
@@ -3,11 +3,29 @@ const http = require('http')
 const server =http.createServer((req,res)=>{
     console.log(req.url,req.headers,req.method);
     res.setHeader('Content-Type','text/html');
-    res.write('<html>');
-    res.write('<head><title>My First Page</title></head>');
-    res.write('<body><h1>Hello from node js server</h1></body>');
-    res.write('</html>');
-    
+    const url=req.url;
+    if(url ==='/home')
+    {
+        res.write('<html>');
+        res.write('<head><title>My First Page</title></head>');
+        res.write('<body><h1>Welcome home</h1></body>');
+        res.write('</html>');
+    }else if(url ==='/about')
+        {
+            res.write('<html>');
+            res.write('<head><title>My First Page</title></head>');
+            res.write('<body><h1>Welcome to About Us page</h1></body>');
+            res.write('</html>');
+        }else if(url ==='/node')
+            {
+                res.write('<html>');
+                res.write('<head><title>My First Page</title></head>');
+                res.write('<body><h1>Welcome to my Node Js project</h1></body>');
+                res.write('</html>');
+        
+            } 
+   
+    res.end();
 });
 
 server.listen(4000);
