c760d24293c3a6f08f1a394f26e6ba390d1009d8
yadavgopinath <yadavgopinath93@gmail.com>
2024-08-16 00:15:28 +0530

    server-creation1

--- my-server/app.js
+++ my-server/app.js
@@ -1,7 +1,13 @@
 const http = require('http')
 
 const server =http.createServer((req,res)=>{
-    console.log("Gopinath Yadav");
+    console.log(req.url,req.headers,req.method);
+    res.setHeader('Content-Type','text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Page</title></head>');
+    res.write('<body><h1>Hello from node js server</h1></body>');
+    res.write('</html>');
+    
 });
 
 server.listen(4000);
